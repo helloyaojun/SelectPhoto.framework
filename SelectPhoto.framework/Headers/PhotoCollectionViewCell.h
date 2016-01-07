84107b4dc9b88088b63342b17bf5666fadf39572
coco <cocoyaojun@163.com>
2016-01-07 17:52:04 +0800

    content
    content

---
+++ SelectPhoto.framework/Headers/PhotoCollectionViewCell.h
@@ -0,0 +1,22 @@
+//
+//  PhotoCollectionViewCell.h
+//  MultiPhotosPicker
+//
+//  Created by yaojun on 15-7-10.
+//  Copyright (c) 2015年 yaojun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#define WIDTH_SCREEN        [[UIScreen mainScreen] bounds].size.width
+#define HEIGHT_SCREEN       [[UIScreen mainScreen] bounds].size.height
+
+#define CELL_MARGIN         6
+
+@interface PhotoCollectionViewCell : UICollectionViewCell
+
+@property (nonatomic,strong)UIImageView *photoImageView;
+@property (nonatomic,assign)BOOL stateSelected;
+
+
+@end
---
+++ SelectPhoto.framework/Headers/SelectPhoto.h
@@ -0,0 +1,19 @@
+//
+//  SelectPhoto.h
+//  SelectPhoto
+//
+//  Created by 姚君 on 16/1/6.
+//  Copyright © 2016年 coco. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SelectPhoto.
+FOUNDATION_EXPORT double SelectPhotoVersionNumber;
+
+//! Project version string for SelectPhoto.
+FOUNDATION_EXPORT const unsigned char SelectPhotoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SelectPhoto/PublicHeader.h>
+
+
